244459d6b327a8dc765a440154298b61c9f786e9
Satya Rohith <me@satyarohith.com>
2020-05-27 12:32:07 +0530

    Revert "fix: use executable nim binary instead of npx"
    This reverts commit 6b58f24319ba24c9eb39c062eb251e2f794d7355.

--- index.js
+++ index.js
@@ -3,17 +3,15 @@ const {join} = require('path');
 
 module.exports = {
   onPostBuild: async ({constants, utils, inputs}) => {
-    const nim = join(__dirname, 'node_modules', '.bin', 'nim');
-
     // Login
     if (process.env.CI) {
       await utils.run.command(
-        `${nim} auth login ${process.env.NIM_TOKEN || inputs.nimbellaToken}`
+        `npx nim auth login ${process.env.NIM_TOKEN || inputs.nimbellaToken}`
       );
     }
 
     // Redirect api calls
-    const {stdout} = await utils.run.command(`${nim} auth current`);
+    const {stdout} = await utils.run.command('npx nim auth current');
     const namespace = stdout.trim();
     const redirectRule = `/api/* https://apigcp.nimbella.io/api/v1/web/${namespace}/default/:splat 200!\n`;
     await appendFile(join(constants.PUBLISH_DIR, '_redirects'), redirectRule);
@@ -25,7 +23,7 @@ module.exports = {
       // Deploy
       console.log(`Deploying ${file}...`);
       let {stderr, exitCode} = await utils.run.command(
-        `${nim} action update ${file.split('.')[0]} ${join(
+        `npx nim action update ${file.split('.')[0]} ${join(
           constants.FUNCTIONS_SRC,
           file
         )} --kind nodejs-lambda:10 --main handler --web=true`,
