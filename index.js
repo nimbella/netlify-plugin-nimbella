3a1c6c4979b70628e2800ecf81332b3ca7043acc
Satya Rohith <me@satyarohith.com>
2020-05-29 00:05:20 +0530

    feat: stop netlify from deploying functions

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
-const {appendFile} = require('fs').promises;
+const {appendFile, readFile, writeFile} = require('fs').promises;
 const {join} = require('path');
+const toml = require('@iarna/toml');
 
 const nim = `npx -p https://apigcp.nimbella.io/downloads/nim/nimbella-cli.tgz nim`;
 
 module.exports = {
-  onPreBuild: async ({constants, utils, inputs}) => {
+  onPreBuild: async ({constants, utils}) => {
     // Login
     if (process.env.NETLIFY) {
       await utils.run.command(
         `${nim} auth login ${process.env.NIM_TOKEN || inputs.nimbellaToken}`
       );
     }
+
+    // Remove functions property to stop netlify from deploying functions.
+    const config = toml.parse(await readFile(constants.CONFIG_PATH));
+    delete config.build.functions;
+    await writeFile(constants.CONFIG_PATH, toml.stringify(config));
   },
   onPostBuild: async ({constants, utils, inputs}) => {
     // Redirect api calls
     const {stdout} = await utils.run.command(`${nim} auth current`);
     const namespace = stdout.trim();
-    const redirectRule = `/api/* https://apigcp.nimbella.io/api/v1/web/${namespace}/default/:splat 200!\n`;
+    const redirectRule = `/.netlify/functions/* https://apigcp.nimbella.io/api/v1/web/${namespace}/default/:splat 200!\n`;
     await appendFile(join(constants.PUBLISH_DIR, '_redirects'), redirectRule);
 
     const {readdir} = require('fs').promises;
