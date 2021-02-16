1e2daffad2d970f3d9aebcfee6b1935a3e1a2da9
Rodric Rabbah <rodric@gmail.com>
2021-02-15 19:42:18 -0500

    Linter.

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
-const {existsSync} = require('fs');
-const {appendFile, readFile, readdir, writeFile} = require('fs').promises;
-const {join} = require('path');
-const {homedir, tmpdir} = require('os');
+const {existsSync} = require('fs')
+const {appendFile, readFile, readdir, writeFile} = require('fs').promises
+const {join} = require('path')
+const {homedir, tmpdir} = require('os')
 
-const toml = require('@iarna/toml');
-const cpx = require('cpx');
-const build = require('netlify-lambda/lib/build');
+const toml = require('@iarna/toml')
+const cpx = require('cpx')
+const build = require('netlify-lambda/lib/build')
 
-const functionsBuildDir = `functions-build-${Date.now()}`;
-const nimConfig = join(homedir(), '.nimbella');
-let netlifyToml = {};
-let isProject = false;
-let isActions = false;
+const functionsBuildDir = `functions-build-${Date.now()}`
+const nimConfig = join(homedir(), '.nimbella')
+let netlifyToml = {}
+let isProject = false
+let isActions = false
 
 // Disable auto updates of nim.
-process.env.NIM_DISABLE_AUTOUPDATE = '1';
+process.env.NIM_DISABLE_AUTOUPDATE = '1'
 
 /**
  * Deploy a Nimbella Project.
  * @param {*} run - function provided under utils by Netlify to build event functions.
  */
 async function deployProject(run) {
-  await run.command(`nim project deploy . --exclude=web`);
+  await run.command(`nim project deploy . --exclude=web`)
 }
 
 /**
@@ -30,36 +30,36 @@ async function deployProject(run) {
  * @param {string} functionsDir - Path to the actions directory.
  */
 async function deployActions({run, functionsDir, timeout, memory, envsFile}) {
-  const files = await readdir(functionsDir);
+  const files = await readdir(functionsDir)
   if (!existsSync(envsFile)) {
-    envsFile = false;
+    envsFile = false
   }
 
   await Promise.all(
     files.map(async (file) => {
-      const [actionName, extension] = file.split('.');
+      const [actionName, extension] = file.split('.')
       const command = [
         `nim action update ${actionName} ${join(functionsDir, file)}`,
         `--timeout=${Number(timeout)} --memory=${Number(memory)} `,
         `--web=raw`
-      ];
+      ]
 
       if (extension === 'js') {
-        command.push('--kind nodejs-lambda:10 --main handler');
+        command.push('--kind nodejs-lambda:10 --main handler')
       }
 
       if (envsFile) {
-        command.push(`--env-file=${envsFile}`);
+        command.push(`--env-file=${envsFile}`)
       }
 
       const {stderr, exitCode, failed} = await run.command(command.join(' '), {
         reject: false,
         stdout: 'ignore'
-      });
-      const message = exitCode === 0 && !failed ? 'done.' : String(stderr);
-      console.log(`Deploying ${file}: ${message}`);
+      })
+      const message = exitCode === 0 && !failed ? 'done.' : String(stderr)
+      console.log(`Deploying ${file}: ${message}`)
     })
-  );
+  )
 }
 
 module.exports = {
@@ -71,85 +71,85 @@ module.exports = {
     ) {
       utils.build.failBuild(
         'Nimbella login token is not available. Please run `netlify addons:create nimbella` at the base of your local project directory linked to your Netlify site.'
-      );
+      )
     }
 
-    await utils.cache.restore(nimConfig);
+    await utils.cache.restore(nimConfig)
 
-    const loggedIn = existsSync(nimConfig);
+    const loggedIn = existsSync(nimConfig)
     // Login if not logged in before.
     if (loggedIn) {
       try {
         const {stdout} = await utils.run.command('nim auth current', {
           stdout: 'pipe'
-        });
-        console.log(`Using the following namespace: ${stdout}`);
+        })
+        console.log(`Using the following namespace: ${stdout}`)
       } catch (error) {
         utils.build.failBuild(
           'Failed to retrieve the current namespace from cache',
           {error}
-        );
+        )
       }
     } else {
       try {
         await utils.run.command(
           `nim auth login ${process.env.NIMBELLA_LOGIN_TOKEN}`
-        );
+        )
         // Cache the nimbella config to avoid logging in for consecutive builds.
-        await utils.cache.save(nimConfig);
+        await utils.cache.save(nimConfig)
       } catch (error) {
         utils.build.failBuild('Failed to login using the provided token', {
           error
-        });
+        })
       }
     }
 
     if (constants.CONFIG_PATH && existsSync(constants.CONFIG_PATH)) {
-      netlifyToml = toml.parse(await readFile(constants.CONFIG_PATH));
+      netlifyToml = toml.parse(await readFile(constants.CONFIG_PATH))
     }
 
-    isActions = inputs.functions ? existsSync(inputs.functions) : false;
-    isProject = existsSync('packages');
+    isActions = inputs.functions ? existsSync(inputs.functions) : false
+    isProject = existsSync('packages')
   },
   // Build the functions
   onBuild: async ({utils, inputs}) => {
     try {
       if (isActions) {
         // Here we're passing the build directory instead of source because source is extracted from inputs.functions.
-        const stats = await build.run(functionsBuildDir, inputs.functions);
-        console.log(stats.toString(stats.compilation.options.stats));
+        const stats = await build.run(functionsBuildDir, inputs.functions)
+        console.log(stats.toString(stats.compilation.options.stats))
         // Copy any files that do not end with .js. T
-        cpx.copy(inputs.functions + '/**/*.!(js)', functionsBuildDir);
+        cpx.copy(inputs.functions + '/**/*.!(js)', functionsBuildDir)
       }
     } catch (error) {
-      utils.build.failBuild('Failed to build the functions', {error});
+      utils.build.failBuild('Failed to build the functions', {error})
     }
   },
   // Execute after build is done.
   onPostBuild: async ({constants, utils, inputs}) => {
-    const {stdout: namespace} = await utils.run.command(`nim auth current`);
+    const {stdout: namespace} = await utils.run.command(`nim auth current`)
 
     if (process.env.CONTEXT === 'production') {
       if (isProject) {
         try {
-          await deployProject(utils.run);
+          await deployProject(utils.run)
         } catch (error) {
-          utils.build.failBuild('Failed to deploy the project', {error});
+          utils.build.failBuild('Failed to deploy the project', {error})
         }
       }
 
       if (isActions) {
-        console.log('\n------------------Functions------------------\n');
+        console.log('\n------------------Functions------------------\n')
         try {
           if (inputs.envs.length > 0) {
-            const envs = {};
-            let envMessage = 'Forwarded the following environment variables: ';
+            const envs = {}
+            let envMessage = 'Forwarded the following environment variables: '
             inputs.envs.forEach((env, index) => {
-              envs[env] = process.env[env];
-              envMessage += index === inputs.envs.length - 1 ? env : env + ', ';
-            });
-            await writeFile(join(tmpdir(), 'env.json'), JSON.stringify(envs));
-            console.log(envMessage);
+              envs[env] = process.env[env]
+              envMessage += index === inputs.envs.length - 1 ? env : env + ', '
+            })
+            await writeFile(join(tmpdir(), 'env.json'), JSON.stringify(envs))
+            console.log(envMessage)
           }
 
           await deployActions({
@@ -158,36 +158,36 @@ module.exports = {
             functionsDir: functionsBuildDir,
             timeout: inputs.timeout, // Default is 6 seconds
             memory: inputs.memory // Default is 256MB (max for free tier)
-          });
+          })
         } catch (error) {
-          utils.build.failBuild('Failed to deploy the functions', {error});
+          utils.build.failBuild('Failed to deploy the functions', {error})
         }
       }
     } else {
       console.log(
         `Skipping the deployment to Nimbella as the context (${process.env.CONTEXT}) is not production.`
-      );
+      )
     }
 
-    const redirectRules = [];
-    const redirects = [];
-    const redirectsFile = join(constants.PUBLISH_DIR, '_redirects');
+    const redirectRules = []
+    const redirects = []
+    const redirectsFile = join(constants.PUBLISH_DIR, '_redirects')
 
     if (isActions) {
       if (existsSync(redirectsFile)) {
         console.log(
           "Found _redirects file. We will rewrite rules that redirect (200 rewrites) to '/.netlify/functions/*'."
-        );
-        const {parseRedirectsFormat} = require('netlify-redirect-parser');
-        const {success} = await parseRedirectsFormat(redirectsFile);
-        redirects.push(...success);
+        )
+        const {parseRedirectsFormat} = require('netlify-redirect-parser')
+        const {success} = await parseRedirectsFormat(redirectsFile)
+        redirects.push(...success)
       }
 
       if (netlifyToml.redirects) {
         console.log(
           "Found redirect rules in netlify.toml. We will rewrite rules that redirect (200 rewrites) to '/.netlify/functions/*'."
-        );
-        redirects.push(...netlifyToml.redirects);
+        )
+        redirects.push(...netlifyToml.redirects)
       }
 
       for (const redirect of redirects) {
@@ -196,38 +196,38 @@ module.exports = {
           redirect.to &&
           redirect.to.startsWith('/.netlify/functions/')
         ) {
-          const redirectPath = redirect.to.split('/.netlify/functions/')[1];
+          const redirectPath = redirect.to.split('/.netlify/functions/')[1]
           redirectRules.push(
             `${redirect.from} https://apigcp.nimbella.io/api/v1/web/${namespace}/default/${redirectPath} 200!`
-          );
+          )
         }
       }
     }
 
-    let {path: redirectPath} = inputs;
+    let {path: redirectPath} = inputs
     redirectPath = redirectPath.endsWith('/')
       ? redirectPath
-      : redirectPath + '/';
+      : redirectPath + '/'
 
     if (isProject) {
       redirectRules.push(
         `${redirectPath}* https://apigcp.nimbella.io/api/v1/web/${namespace}/:splat 200!`
-      );
+      )
     }
 
     if (isActions && !isProject) {
       redirectRules.push(
         `${redirectPath}* https://apigcp.nimbella.io/api/v1/web/${namespace}/default/:splat 200!`
-      );
+      )
     }
 
     if (redirectRules.length > 0) {
       if (!existsSync(constants.PUBLISH_DIR)) {
-        const mkdir = require('make-dir');
-        await mkdir(constants.PUBLISH_DIR);
+        const mkdir = require('make-dir')
+        await mkdir(constants.PUBLISH_DIR)
       }
 
-      await appendFile(redirectsFile, redirectRules.join('\n'));
+      await appendFile(redirectsFile, redirectRules.join('\n'))
     }
   }
-};
+}
--- test/index.js
+++ test/index.js
@@ -1,9 +1,9 @@
-const path = require('path');
+const path = require('path')
 
-const mockFs = require('mock-fs');
-const build = require('netlify-lambda/lib/build');
+const mockFs = require('mock-fs')
+const build = require('netlify-lambda/lib/build')
 
-const plugin = require('../src');
+const plugin = require('../src')
 
 const utils = {
   cache: {
@@ -17,114 +17,114 @@ const utils = {
   build: {
     failBuild: jest.fn()
   }
-};
-jest.mock('netlify-lambda/lib/build');
-console.log = jest.fn();
+}
+jest.mock('netlify-lambda/lib/build')
+console.log = jest.fn()
 
 afterEach(() => {
-  delete require.cache[require.resolve('../src')];
-  utils.build.failBuild.mockReset();
-  utils.run.command.mockReset();
-  utils.cache.has.mockReset();
-  utils.cache.restore.mockReset();
-  utils.cache.save.mockReset();
-  build.run.mockReset();
-  console.log.mockReset();
-});
+  delete require.cache[require.resolve('../src')]
+  utils.build.failBuild.mockReset()
+  utils.run.command.mockReset()
+  utils.cache.has.mockReset()
+  utils.cache.restore.mockReset()
+  utils.cache.save.mockReset()
+  build.run.mockReset()
+  console.log.mockReset()
+})
 
 describe('preBuild()', () => {
   test('show token not available message when login token not set', async () => {
     // Prepare
-    process.env.NIMBELLA_LOGIN_TOKEN = '';
-    utils.cache.has.mockResolvedValue(false);
+    process.env.NIMBELLA_LOGIN_TOKEN = ''
+    utils.cache.has.mockResolvedValue(false)
 
     await plugin.onPreBuild({
       utils,
       constants: {},
       inputs: {}
-    });
+    })
 
-    expect(utils.cache.has.mock.calls.length).toBe(1);
+    expect(utils.cache.has.mock.calls.length).toBe(1)
     expect(utils.build.failBuild.mock.calls[0][0]).toEqual(
       'Nimbella login token is not available. Please run `netlify addons:create nimbella` at the base of your local project directory linked to your Netlify site.'
-    );
-  });
+    )
+  })
 
   test('Show the current namespace if already logged in', async () => {
     // Prepare
-    process.env.NIMBELLA_LOGIN_TOKEN = 'somevalue';
+    process.env.NIMBELLA_LOGIN_TOKEN = 'somevalue'
     utils.run.command.mockReturnValue({
       stdout: 'namespace'
-    });
+    })
     const mockFiles = {
       [path.join(require('os').homedir(), '.nimbella')]: {}
-    };
+    }
 
-    mockFs(mockFiles);
+    mockFs(mockFiles)
     await plugin.onPreBuild({
       utils,
       constants: {},
       inputs: {}
-    });
-    mockFs.restore();
+    })
+    mockFs.restore()
 
-    expect(utils.cache.restore.mock.calls.length).toBe(1);
-    expect(utils.run.command.mock.calls[0][0]).toEqual(`nim auth current`);
+    expect(utils.cache.restore.mock.calls.length).toBe(1)
+    expect(utils.run.command.mock.calls[0][0]).toEqual(`nim auth current`)
     expect(console.log.mock.calls[0][0]).toEqual(
       'Using the following namespace: namespace'
-    );
-  });
-});
+    )
+  })
+})
 
 describe('onBuild()', () => {
   test('should build functions if functions input is set in config ', async () => {
-    process.env.NIMBELLA_LOGIN_TOKEN = 'somevalue';
+    process.env.NIMBELLA_LOGIN_TOKEN = 'somevalue'
 
     mockFs({
       functions: {},
       'netlify.toml': ''
-    });
+    })
     await plugin.onPreBuild({
       utils,
       constants: {CONFIG_PATH: './netlify.toml'},
       inputs: {functions: 'functions'}
-    });
+    })
     await plugin.onBuild({
       utils,
       inputs: {functions: 'functions'}
-    });
-    mockFs.restore();
+    })
+    mockFs.restore()
 
-    expect(build.run.mock.calls[0][1]).toEqual('functions');
-  });
+    expect(build.run.mock.calls[0][1]).toEqual('functions')
+  })
 
   test('should not build functions if set functions directory is not present', async () => {
     mockFs({
       packages: {},
       'netlify.toml': ''
-    });
+    })
     await plugin.onPreBuild({
       utils,
       constants: {CONFIG_PATH: './netlify.toml'},
       inputs: {functions: 'abcd'}
-    });
+    })
     await plugin.onBuild({
       utils,
       inputs: {functions: 'abcd'}
-    });
-    mockFs.restore();
+    })
+    mockFs.restore()
 
-    expect(build.run.mock.calls.length).toBe(0);
-  });
-});
+    expect(build.run.mock.calls.length).toBe(0)
+  })
+})
 
 describe('onPostBuild()', () => {
   test('should skip deployment if the context is not production', async () => {
-    process.env.NIMBELLA_LOGIN_TOKEN = 'somevalue';
-    process.env.CONTEXT = 'dev';
+    process.env.NIMBELLA_LOGIN_TOKEN = 'somevalue'
+    process.env.CONTEXT = 'dev'
     utils.run.command.mockReturnValue({
       stdout: 'namespace'
-    });
+    })
     const pluginInputs = {
       utils,
       constants: {CONFIG_PATH: './netlify.toml', PUBLISH_DIR: ''},
@@ -132,23 +132,23 @@ describe('onPostBuild()', () => {
         path: '',
         functions: ''
       }
-    };
+    }
 
-    mockFs({});
-    await plugin.onPostBuild(pluginInputs);
-    mockFs.restore();
+    mockFs({})
+    await plugin.onPostBuild(pluginInputs)
+    mockFs.restore()
 
     expect(console.log.mock.calls[0][0]).toEqual(
       `Skipping the deployment to Nimbella as the context (${process.env.CONTEXT}) is not production.`
-    );
-  });
+    )
+  })
 
   test("should run nim project deploy if 'packages' directory exists", async () => {
-    process.env.NIMBELLA_LOGIN_TOKEN = 'somevalue';
-    process.env.CONTEXT = 'production';
+    process.env.NIMBELLA_LOGIN_TOKEN = 'somevalue'
+    process.env.CONTEXT = 'production'
     utils.run.command.mockReturnValue({
       stdout: 'namespace'
-    });
+    })
     const pluginInputs = {
       utils,
       constants: {CONFIG_PATH: './netlify.toml', PUBLISH_DIR: ''},
@@ -156,26 +156,26 @@ describe('onPostBuild()', () => {
         path: '',
         functions: ''
       }
-    };
+    }
 
     mockFs({
       packages: {}
-    });
-    await plugin.onPreBuild(pluginInputs);
-    await plugin.onPostBuild(pluginInputs);
-    mockFs.restore();
+    })
+    await plugin.onPreBuild(pluginInputs)
+    await plugin.onPostBuild(pluginInputs)
+    mockFs.restore()
 
     expect(utils.run.command.mock.calls[2][0]).toEqual(
       `nim project deploy . --exclude=web`
-    );
-  });
+    )
+  })
 
   test('should rewrite existing redirects to .netlify/functions/ in netlify.toml if functions are used', async () => {
-    process.env.NIMBELLA_LOGIN_TOKEN = 'somevalue';
-    process.env.CONTEXT = 'production';
+    process.env.NIMBELLA_LOGIN_TOKEN = 'somevalue'
+    process.env.CONTEXT = 'production'
     utils.run.command.mockReturnValue({
       stdout: 'namespace'
-    });
+    })
     const pluginInputs = {
       utils,
       constants: {CONFIG_PATH: 'netlify.toml', PUBLISH_DIR: ''},
@@ -183,7 +183,7 @@ describe('onPostBuild()', () => {
         path: '',
         functions: 'some-dir'
       }
-    };
+    }
 
     mockFs({
       'netlify.toml': `
@@ -196,12 +196,12 @@ describe('onPostBuild()', () => {
         'create.js': '',
         'update.js': ''
       }
-    });
-    await plugin.onPreBuild(pluginInputs);
-    await plugin.onPostBuild(pluginInputs);
-    mockFs.restore();
+    })
+    await plugin.onPreBuild(pluginInputs)
+    await plugin.onPostBuild(pluginInputs)
+    mockFs.restore()
     expect(console.log.mock.calls[1][0]).toEqual(
       "Found redirect rules in netlify.toml. We will rewrite rules that redirect (200 rewrites) to '/.netlify/functions/*'."
-    );
-  });
-});
+    )
+  })
+})
